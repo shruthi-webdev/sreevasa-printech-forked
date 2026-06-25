b06477b48ec0e68ec77a68b1602a793b90df1462
Shruthi V <shruthisvenkatesan@outlook.com>
2026-06-25 12:15:49 +0530

    http cahnge

--- printech-app/vite.config.js
+++ printech-app/vite.config.js
@@ -1,13 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import basicSsl from '@vitejs/plugin-basic-ssl'
 
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [react(), basicSsl()],
+    plugins: [react()],
     server: {
         host: true,
-        https: true,
         proxy: {
             '/vasa_wo_api': {
                 target: process.env.VITE_API_PROXY_TARGET || 'http://117.218.59.130:8080',
